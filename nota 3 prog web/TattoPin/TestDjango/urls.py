32078ea6d9f9854e2d98b9100a3c256761fb38bf
JohnLiberona <134554189+JohnLiberona@users.noreply.github.com>
2025-01-27 13:35:19 -0300

    Add files via upload

---
+++ nota 3 prog web/TattoPin/TestDjango/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from api.api import UserApi
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('api/', include('api.urls')),
+    path('api/create_user', UserApi.as_view(), name="api_CreateUser"),
+    path('account/', include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
