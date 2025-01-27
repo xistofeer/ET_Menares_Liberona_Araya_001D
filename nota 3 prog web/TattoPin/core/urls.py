e22bc6d5024e52fdf72532ed9a22e90cb90fba18
JohnLiberona <134554189+JohnLiberona@users.noreply.github.com>
2025-01-27 13:37:06 -0300

    Add files via upload

---
+++ nota 3 prog web/TattoPin/core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import menu, acercade, carrousel, formulario, galeria, login_view, django_view, form_tattoo, eliminar, modificar, listado, tienda
+
+urlpatterns = [
+    path('', menu, name='menu'),          # Ruta para la vista del menú (Página principal)
+    path('acercade/', acercade, name='acercade'),  # Ruta para la vista de "acerca de"
+    path('carrousel/', carrousel, name='carrousel'),  # Ruta para el carrusel
+    path('formulario/', formulario, name='formulario'),  # Ruta para el formulario
+    path('galeria/', galeria, name='galeria'),  # Ruta para la galería
+    path('login/', login_view, name='login'),  # Ruta para el login
+    path('django/', django_view, name='django' ),
+    path('form_tattoo', form_tattoo, name='form_tattoo' ),
+    path('eliminar/<id>', eliminar, name="eliminar"),
+    path('modificar/<id>', modificar, name="modificar"),
+    path('listado/',listado, name='listado'),
+    path('tienda/',tienda , name='tienda'),
+]
