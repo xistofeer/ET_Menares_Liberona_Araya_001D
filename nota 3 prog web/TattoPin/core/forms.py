1df2357f622a338a6ab64e020ddf81b595636cbe
JohnLiberona <134554189+JohnLiberona@users.noreply.github.com>
2025-01-29 19:26:55 -0300

    Actualizacion2

---
+++ nota 3 prog web/TattoPin/core/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Tattoo, Categoria
+
+class TattooForm(forms.ModelForm):
+    class Meta: 
+        model = Tattoo
+        fields = [ 'codigo', 'titulo', 'tipo', 'categoria', 'precio','imagen']
+        labels = {
+            'codigo': 'codigo',
+            'titulo' : 'titulo',
+            'tipo' : 'tipo',
+            'categoria' : 'Categoria',
+            'precio' : 'precio',
+            'imagen': 'Imagen'
+        }
+        widgets ={
+            'codigo': forms.TextInput(
+                attrs={
+                    'placeholder':'Ingrese codigo..',
+                    'id': 'codigo',
+                    'class': 'form-control'
+                }
+            ),
+            'titulo': forms.TextInput(
+                attrs={
+                    'placeholder': 'Ingrese titulo..',
+                    'id': 'titulo',
+                    'class': 'form-control'
+                }
+            ),
+            'tipo': forms.TextInput(
+                attrs={
+                    'placeholder': 'Ingrese tipo..',
+                    'id': 'tipo',
+                    'class': 'form-control'
+                }
+            ),
+            'categoria': forms.Select(
+                attrs={
+                    'id': 'categoria',
+                    'class': 'form-control'
+                }
+            ), 
+            'precio': forms.NumberInput(
+                attrs={
+                    'placeholder': 'Ingrese precio',
+                    'id': 'precio',
+                    'class': 'form-control'
+                }
+            ),
+            'imagen': forms.FileInput(
+                attrs={
+                    'class': 'form-control',
+                    'id': 'imagen',
+                }
+            )
+            
+        }
