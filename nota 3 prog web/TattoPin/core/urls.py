e20666d99bd4d308af41337ab867e790e8a714b6
JohnLiberona <134554189+JohnLiberona@users.noreply.github.com>
2025-01-29 21:03:02 -0300

    Update urls.py

--- nota 3 prog web/TattoPin/core/urls.py
+++ nota 3 prog web/TattoPin/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import menu, acercade, carrousel, formulario, galeria, login_view, django_view, form_tattoo, eliminar, modificar, listado, tienda
+from .views import menu, acercade, carrousel, formulario, galeria, login_view, django_view, form_tattoo, eliminar, modificar, listado, carrito ,tienda, agregar_al_carrito, eliminar_del_carrito
 
 urlpatterns = [
     path('', menu, name='menu'),          # Ruta para la vista del menú (Página principal)
@@ -11,7 +11,11 @@ urlpatterns = [
     path('django/', django_view, name='django' ),
     path('form_tattoo', form_tattoo, name='form_tattoo' ),
     path('eliminar/<id>', eliminar, name="eliminar"),
-    path('modificar/<id>', modificar, name="modificar"),
+    path('modificar/<id>', modificar, name='modificar'),
     path('listado/',listado, name='listado'),
+
     path('tienda/',tienda , name='tienda'),
+    path('carrito/', carrito, name='carrito'),
+    path('agregar-al-carrito/<str:tattoo_id>/', agregar_al_carrito, name='agregar_al_carrito'),
+    path('eliminar-del-carrito/<str:tattoo_id>/', eliminar_del_carrito, name='eliminar_del_carrito'),    
 ]
