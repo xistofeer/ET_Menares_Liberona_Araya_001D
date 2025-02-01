3ecc531a6baf03f75ce00df7034bcd7701d862c5
JohnLiberona <134554189+JohnLiberona@users.noreply.github.com>
2025-01-31 21:10:20 -0300

    Delete nota 3 prog web/TattoPinCarrito directory

--- nota 3 prog web/TattoPinCarrito/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PersonaList
-
-urlpatterns = [
-    path('persona/', PersonaList.as_view(), name='persona_list'),
-]
--- nota 3 prog web/TattoPinCarrito/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Categoria,Tattoo
-# Register your models here.
-
-
-admin.site.register(Categoria)
-admin.site.register(Tattoo)
