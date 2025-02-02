2fdabb8a6bd654b332a9cba1c68904832ee9104d
xistofeer <90876612+xistofeer@users.noreply.github.com>
2025-02-02 17:42:42 -0300

    Add files via upload

---
+++ nota final prog web/TattoPinV1.0/api/api.py
@@ -0,0 +1,17 @@
+from rest_framework import response
+from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework import status
+
+
+class UserApi (APIView):
+
+    def post (self, request):
+
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            return response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return response(serializer.data, status = status.HTTP_400_BAD_REQUEST)
