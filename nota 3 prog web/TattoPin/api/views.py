fdc43495638a25e928737744eebc7436439ac665
xistofeer <90876612+xistofeer@users.noreply.github.com>
2025-02-02 17:37:02 -0300

    Delete nota 3 prog web directory
    se actualiza

--- nota 3 prog web/TattoPin/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import Persona
-from .serializers import PersonaSerializer
-
-class PersonaList(generics.ListCreateAPIView):
-    queryset = Persona.objects.all()
-    serializer_class = PersonaSerializer    
--- nota 3 prog web/TattoPin95%/core/static/js/tooltip.js
+++
@@ -1,7 +0,0 @@
-console.log("Tooltip.js cargado correctamente");
-document.addEventListener("DOMContentLoaded", function () {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-  });
-});
